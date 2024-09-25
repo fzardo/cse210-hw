661a1a86e8f3bdd179246d6f37f563e33def5b94
fzardo <f.zardo@outlook.com>
2024-09-25 07:07:15 -0300

    Week02 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Company 1";
+        job1._jobTitle = "Customer Experience Agent";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Company 2";
+        job2._jobTitle = "Customer Experience Agent";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Felipe Zardo Cabral";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
